28e73dec2fbadbaa41010219a686c7bf9024ef6a
Ren <rennyandres@gmail.com>
2018-06-21 11:26:47 +0000

    Basic functionality

--- app.js
+++ app.js
@@ -3,11 +3,15 @@
 // IMPORTS --------------------------------------------------------------------
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var upload = require('express-fileupload');
 
 
 // CONFIG ---------------------------------------------------------------------
 app.set('view engine', 'ejs');
 app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(upload());
 
 
 // ROUTES ---------------------------------------------------------------------
@@ -15,10 +19,24 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
+app.post('/api/readthisfile', function(req, res) {
+    if(req.files) {
+        res.send({
+            "name": req.files.file.name,
+            "type": req.files.file.mimetype,
+            "size": req.files.file.data.length
+        });
+    }
+    else {
+        res.json({
+            "error" : 'No file was found' 
+        })
+    }
+});
+
+
 
 // LISTENING ------------------------------------------------------------------
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log('Serving on port ' + process.env.PORT);
 });
-
-
